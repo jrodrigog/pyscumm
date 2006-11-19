40c0c6769362f487cf906fc5585a9119abb7d16f
jrodrigog <jrodrigog@e981367f-a619-0410-afbf-337fa432c55d>
2006-11-19 09:07:53 +0000

    Update for testing Picking

--- test/maintest.py
+++ test/maintest.py
@@ -5,23 +5,6 @@ class Taverna( pyscumm.scene.Scene ):
         self._state = Taverna1()
 
 class Taverna1( pyscumm.scene.SceneState ):
-
-    def on_quit( self ):
-        pyscumm.vm.VM().display.close()
-        sys.exit()
-        return self
-
-    def on_key_down( self, key ):
-        print 'WAU!, se ha pulsado en mi juego la tecla', key
-        return self
-
-    def on_key_up( self, key ):
-        print 'WAU!, se ha soltado en mi juego la tecla', key
-        return self
-
-    def on_mouse_click( self, button, obj ):
-        print 'WAU!, se ha pulsado en mi juego el botoon', button
-        return self
-
+    pass
 
 pyscumm.vm.VM.boot( Taverna() )
