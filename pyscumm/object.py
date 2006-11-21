cf4fe1d7888d8ae70c26680e45a18fd005974d44
kernel.no.found <kernel.no.found@e981367f-a619-0410-afbf-337fa432c55d>
2006-11-21 23:02:22 +0000

    test/main_test.py edited pyscumm/object.py edited

--- pyscumm/object.py
+++ pyscumm/object.py
@@ -7,9 +7,9 @@ class Object( dict ):
         self._box = box.Box()
         self._box.location[0] = 320.; self._box.location[1] = 240.
         self._box.box[0][0] = -200.; self._box.box[0][1] = 200.
-        self._box.box[1][0] =  200.; self._box.box[1][1] = 200.
+        self._box.box[1][0] =  -200.; self._box.box[1][1] = -200.
         self._box.box[2][0] =  200.; self._box.box[2][1] =  -200.
-        self._box.box[3][0] = -200.; self._box.box[3][1] =  -200.
+        self._box.box[3][0] = 200.; self._box.box[3][1] =  200.
 
     def get_box( self ):
         return self._box
