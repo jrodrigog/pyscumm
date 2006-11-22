ead59c5427af9a2f76139b7e3f560bd1478f6ea9
jrodrigog <jrodrigog@e981367f-a619-0410-afbf-337fa432c55d>
2006-11-22 00:22:56 +0000

    Done

--- test/main_test.py
+++ test/main_test.py
@@ -1,18 +1,24 @@
 import pyscumm, sys
 
 class MyObject( pyscumm.object.Object ):
-    pass
+    def __init__( self ):
+        box = pyscumm.box.Box()
+        box.location[0] = 320.; box.location[1] = 240.
+        box.box[0][0] = -200.; box.box[0][1] = -200.
+        box.box[1][0] =  200.; box.box[1][1] = -200.
+        box.box[2][0] =  200.; box.box[2][1] =  200.
+        box.box[3][0] = -200.; box.box[3][1] =  200.
+        self[ "box" ] = box
+    def collides( self, obj ):
+        return self["box"].collides( obj )
 
 class Taverna( pyscumm.scene.Scene ):
     def start( self ):
         self._state = Taverna1()
-        self[ 'obj' ] = MyObject()
+        self[ "obj" ] = MyObject()
 
 class Taverna1( pyscumm.scene.SceneState ):
     pass
-
-
-
 
 pyscumm.vm.VM().state = pyscumm.vm.NormalMode()
 pyscumm.vm.VM.boot( Taverna() )
