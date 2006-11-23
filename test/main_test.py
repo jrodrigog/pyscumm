eec1bde123dead55ee484e3fc63482ae10b0f5bb
jrodrigog <jrodrigog@e981367f-a619-0410-afbf-337fa432c55d>
2006-11-23 07:21:50 +0000

    Visual grab

--- test/main_test.py
+++ test/main_test.py
@@ -27,7 +27,8 @@ class MyObject( pyscumm.object.Object ):
         self._box.box[3][0] = -50.; self._box.box[3][1] =  50.
         self["id"] = self.__class__.count
         self.__class__.count += 1
-
+    def draw( self ):
+        self._box.draw()
     def collides( self, obj ):
         return self._box.collides( obj )
     def __cmp__( self, obj ):
@@ -36,21 +37,50 @@ class MyObject( pyscumm.object.Object ):
     box = property( get_box )
 
 class Taverna( pyscumm.scene.Scene ):
+    N = 16
     def start( self ):
         self._state = Taverna1()
-
-        for i in xrange( 64 ):
+        self._dragging = None
+        self._offset = None
+        for i in xrange( self.N ):
             x = MyObject()
             #x[ 'box' ].location[0] = random.random() * 640
             #x[ 'box' ].location[1] = random.random() * 320
             x.box.location[0] = random.random() * 640
             x.box.location[1] = random.random() * 320
-            x.box.location[2] = random.random()
+            x.box.location[2] = float( i ) / self.N
+            x.box.rgb = [ random.random(), random.random(), random.random() ]
             x.box.update()
             self[ id(x) ] = x
+    def get_dragging( self ): return self._dragging
+    def set_dragging( self, dragging ): self._dragging = dragging
+    def get_offset( self ): return self._offset
+    def set_offset( self, offset ): self._offset = offset
+    dragging = property( get_dragging, set_dragging )
+    offset = property( get_offset, set_offset )
 
 class Taverna1( pyscumm.scene.SceneState ):
-    pass
+    _shared_state = {}
+    LEFT = 1
+    def __init__( self ):
+        self.__dict__ = self._shared_state
+        if self.__dict__: return
+        pyscumm.scene.SceneState.__init__( self )
+    def on_mouse_motion( self, loc ):
+        if not self.scene.dragging: return self
+        self.scene.dragging.box.location = loc - self.scene.offset
+        self.scene.dragging.box.update()
+        return self
+    def on_mouse_drag_start( self, obj, loc, button ):
+        if button != self.LEFT or not len( obj ): return self
+        # Use the top object
+        self.scene.dragging = obj.pop()
+        self.scene.offset = loc - self.scene.dragging.box.location
+        return self
+    def on_mouse_drag_end( self, obj, loc, button ):
+        self.scene.dragging = None
+        return self
+
 
 pyscumm.vm.VM().state = pyscumm.vm.NormalMode()
 pyscumm.vm.VM.boot( Taverna(), pyscumm.gfx.gl.Display() )
