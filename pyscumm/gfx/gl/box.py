1029b68ade57e9d0c8cd2e93228100b4d1b612f5
jrodrigog <jrodrigog@e981367f-a619-0410-afbf-337fa432c55d>
2006-11-24 04:09:45 +0000

    Update color

--- pyscumm/gfx/gl/box.py
+++ pyscumm/gfx/gl/box.py
@@ -12,7 +12,7 @@ class Box( pyscumm.box.Box, Object ):
     def __init__( self, shadow=1., depth=1. ):
         Object.__init__( self )
         pyscumm.box.Box.__init__( self, shadow, depth  )
-        self._color = pyscumm.vector.Vector4D( [ 1., 0., 0., 0.25 ] )
+        self._color = pyscumm.vector.Vector4D( [ 1., 0., 0., 0.5 ] )
         self._base = None
         self.update()
 
