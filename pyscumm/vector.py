ac2c16dfa9ebda9143098f2cce3a80ee4e5cb498
jrodrigog <jrodrigog@e981367f-a619-0410-afbf-337fa432c55d>
2006-11-25 04:03:10 +0000

    Rotation added

--- pyscumm/vector.py
+++ pyscumm/vector.py
@@ -33,7 +33,7 @@ class Vector2D( list ):
     def __isub__( self, o ):
         self[0] -= o[0]
         self[1] -= o[1]
-        return self    
+        return self
     def __mult__( self, k ):
         return Vector3D( [
             self[0] * k,
@@ -51,17 +51,12 @@ class Vector2D( list ):
         return Vector2D( [
             self[0] * scale,
             self[1] * scale ] )
-    def rotate( self, alpha ):
-        return self.rotate_z( alpha )
-    def rotate_z( self, alpha ):
-        """
-        x2 = x1*cos(alpha)-y1*sin(alpha)
-        y2 = y1*cos(alpha)+x1*sin(alpha)
-        """
-        x1, y1 = self
-        return Vector2D( [
-            x1 * math.cos( alpha ) - y1 * math.sin( alpha ),
-            y1 * math.cos( alpha ) + x1 * math.sin( alpha ) ] )
+    def rotate( self, rotation ):
+        if not isinstance( rotation, VectorRotation ):
+            rotation = RotateVector( rotation )
+        return rotation.rotate( self )
+    def __repr__( self ):
+        return "Vector2D([%.2f,%.2f])" % ( self[0], self[1] )
     deserialize = classmethod( deserialize )
 
 class Vector3D( list ):
@@ -128,21 +123,14 @@ class Vector3D( list ):
             self[0] * scale,
             self[1] * scale,
             self[2] * scale ] )
-    def rotate( self, alpha ):
-        return self.rotate_z( alpha )
-    def rotate_z( self, alpha ):
-        """
-        x2 = x1*cos(alpha)-y1*sin(alpha)
-        y2 = y1*cos(alpha)+x1*sin(alpha)
-        z2 = z1
-        """
-        x1, y1, z1 = self
-        return Vector3D( [
-            x1 * math.cos( alpha ) - y1 * math.sin( alpha ),
-            y1 * math.cos( alpha ) + x1 * math.sin( alpha ),
-            z1 ] )
+    def rotate( self, rotation ):
+        if not isinstance( rotation, VectorRotation ):
+            rotation = RotateVector( rotation )
+        return rotation.rotate( self )
+    def __repr__( self ):
+        return "Vector3D([%.2f,%.2f,%.2f])" % ( self[0], self[1], self[2] )
     deserialize = classmethod( deserialize )
-        
+
 class Vector4D( list ):
     """4D Vector class"""
     def __init__( self, v = [0.,0.,0.,0.] ):
@@ -219,8 +207,157 @@ class Vector4D( list ):
             self[1] * scale,
             self[2] * scale,
             self[3] * scale ] )
-    def rotate( self, alpha ):
+    def rotate( self, rotation ):
         raise NotImplementedError
-    def rotate_z( self, alpha ):
-        raise NotImplementedError
+    def __repr__( self ):
+        return "Vector4D([%.2f,%.2f,%.2f,%.2f])" % ( self[0], self[1], self[2], self[3] )
     deserialize = classmethod( deserialize )
+
+class VectorRotation( list ):
+    """This abstract class implements a common rotation for
+    2D and 3D vectors based on the current rotation matrix.
+    Reference:
+    http://www.kwon3d.com/theory/transform/rot.html"""
+    def rotate( self, vector ):
+        """Rotates a 2D or 3D vector by a rotation matrix.
+        @param vector: The vector to rotate
+        @type vector: Vector2D/Vector3D
+        @return: A new rotated vector
+        @rtype: Vector2D/Vector3D
+        """
+        v = vector.clone()
+        v[0] = ( self[0][0] * vector[0] ) \
+            + ( self[0][1] * vector[1] )
+        v[1] = ( self[1][0] * vector[0] ) \
+            + ( self[1][1] * vector[1] )
+        # handle 3D/2D
+        try:
+            v[0] += self[0][2] * vector[2]
+            v[1] += self[1][2] * vector[2]
+            v[2] = ( self[2][0] * vector[0] ) \
+                + ( self[2][1] * vector[1] ) \
+                + ( self[2][2] * vector[2] )
+        except IndexError: pass
+        return v
+
+class RotateVectorZ( VectorRotation ):
+    """
+    Rotate a Vector in the Z axis
+    [ cos -sin   0 ]
+    [ sin  cos   0 ]
+    [   0    0   1 ]
+    """
+    def __init__( self, alpha ):
+        """Build a RotateVectorZ object, inits the rotation matrix.
+        @param alpha: Rotation angle in degrees
+        @type alpha: float
+        """
+        cos = math.cos( alpha )
+        sin = math.sin( alpha )
+        VectorRotation.__init__( self,
+            [[ cos, -sin, 0. ],
+             [ sin,  cos, 0. ],
+             [  0.,   0., 1. ]] )
+
+class RotateVectorX( VectorRotation ):
+    """
+    Rotate a Vector in the X axis.
+    [   1    0    0 ]
+    [   0  cos -sin ]
+    [   0  sin  cos ]
+    """
+    def __init__( self, alpha ):
+        """Build a RotateVectorX object, inits the rotation matrix.
+        @param alpha: Rotation angle in degrees
+        @type alpha: float
+        """
+        cos = math.cos( alpha )
+        sin = math.sin( alpha )
+        VectorRotation.__init__( self,
+            [[  1.,  0.,   0. ],
+             [  0., cos, -sin ],
+             [  0., sin,  cos ]] )
+
+class RotateVectorY( VectorRotation ):
+    """
+    Rotate a Vector in the Y axis.
+    [ cos   0  sin ]
+    [   0   1    0 ]
+    [-sin   0  cos ]
+    """
+    def __init__( self, alpha ):
+        """Build a RotateVectorY object, inits the rotation matrix.
+        @param alpha: Rotation angle in degrees
+        @type alpha: float
+        """
+        cos = math.cos( alpha )
+        sin = math.sin( alpha )
+        VectorRotation.__init__( self,
+            [[  cos, 0., sin ],
+             [   0., 1., 0.  ],
+             [ -sin, 0., cos ]] )
+
+class RotateAxisZ( VectorRotation ):
+    """
+    Rotate the Z Axis.
+    [ cos sin   0 ]
+    [-sin cos   0 ]
+    [   0   0   1 ]
+    """
+    def __init__( self, alpha ):
+        """Build a RotateAxisZ object, inits the rotation matrix.
+        @param alpha: Rotation angle in degrees
+        @type alpha: float
+        """
+        cos = math.cos( alpha )
+        sin = math.sin( alpha )
+        VectorRotation.__init__( self,
+            [[  cos, sin, 0. ],
+             [ -sin, cos, 0. ],
+             [   0., 0.,  1. ]] )
+
+class RotateAxisX( VectorRotation ):
+    """
+    Rotate the X Axis.
+    [  1    0   0 ]
+    [  0  cos sin ]
+    [  0 -sin cos ]
+    """
+    def __init__( self, alpha ):
+        """Build a RotateAxisX object, inits the rotation matrix.
+        @param alpha: Rotation angle in degrees
+        @type alpha: float
+        """
+        cos = math.cos( alpha )
+        sin = math.sin( alpha )
+        VectorRotation.__init__( self,
+            [[ 1.,  0.,  0. ],
+             [ 0., cos, sin ],
+             [ 0.,-sin, cos ]] )
+
+class RotateAxisY( VectorRotation ):
+    """
+    Rotate the Y Axis.
+    [ cos   0 -sin ]
+    [  0    1    0 ]
+    [ sin   0  cos ]
+    """
+    def __init__( self, alpha ):
+        """Build a RotateAxisY object, inits the rotation matrix.
+        @param alpha: Rotation angle in degrees
+        @type alpha: float
+        """
+        cos = math.cos( alpha )
+        sin = math.sin( alpha )
+        VectorRotation.__init__( self,
+            [[ cos, 0., -sin ],
+             [  0., 1.,   0. ],
+             [ sin, 0.,  sin ]] )
+
+class RotateVector( RotateVectorZ ):
+    """This is a default 2D Vector rotation (In the Z axis)."""
+    pass 
+
+class RotateAxis( RotateAxisZ ):
+    """This is a default 2D Axis rotation (In the Z axis)."""
+    pass
