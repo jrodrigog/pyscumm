ad7a805f0f15f31dc4045a42cbeb900f1d3098d7
kernel.no.found <kernel.no.found@e981367f-a619-0410-afbf-337fa432c55d>
2006-12-04 01:54:04 +0000

    pyscumm strict Object-Oriented moved to branches

--- pyscumm/gfx/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# pyscumm gfx Package
-
-__version__ = '0.1D'
-
-from base import *
-import gl
--- pyscumm/gfx/gl/__init__.py
+++
@@ -1,4 +0,0 @@
-from base import *
-from box import Box
-from texture import Texture
-from image import Image
